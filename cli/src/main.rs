a32135830a7319f31ff0c9e048d8ccb6be0be77a
Swaye Chateau <57335019+swayechateau@users.noreply.github.com>
2025-05-24 22:57:38 +0100

    updated sysinfo

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,7 +9,7 @@ mod schema;
 mod print;
 #[macro_use]
 mod libs;
-use model::{categories, config, package_manager, system::{self, SystemInfo}};
+use model::{categories, config, package_manager, system};
 use clap::{Parser};
 use args::{handle_arg_errors, handle_args, TranquilityArgs};
 use print::tranquility_figlet;
@@ -17,9 +17,6 @@ use logger::{log_event};
 
 fn main() {
     tranquility_figlet();
-    let sys = SystemInfo::new();
-    println!("{}", sys.to_pretty_string());
-
     log_event("info", "Starting", "tranquility", "success", None);
     match TranquilityArgs::try_parse() {
         Ok(args) => {
